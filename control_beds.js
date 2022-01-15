f77b90d6b323a82e67597d2598a811c1e2972504
Sean Buckley <sean.bck@gmail.com>
2022-01-15 01:20:17 -0500

    add beds

---
+++ control_beds.js
@@ -0,0 +1,27 @@
+L.control.beds = function (opts) {
+
+    let ctrl = L.Control.extend({
+        onAdd: map => {
+            var c = L.DomUtil.create('select');
+            c.innerHTML='<option>Select a sign...</option>';
+
+            L.DomEvent.on(c, 'change', e => {
+                let coord = e.target.value.split(',');
+                map.setView([-parseInt(coord[1]), parseInt(coord[0])]);
+            });
+
+            for (let b of opts.beds.sort((a, b) => a.time - b.time)) {
+                let op = L.DomUtil.create('option');
+                op.value = b.x + ',' + b.z;
+                op.textContent =
+                    new Date(b.time * 1000).toLocaleDateString() +
+                    ` - (${b.x}, ${b.z})`;
+                c.appendChild(op);
+            }
+
+            return c;
+        }
+    });
+
+    return new ctrl(opts);
+};
