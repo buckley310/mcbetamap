842144e82c7d3d4a40be18ff0619312b07ad703d
Sean Buckley <sean.bck@gmail.com>
2022-01-15 01:00:46 -0500

    make custom controls nicer

--- control_spawn.js
+++ control_spawn.js
@@ -1,13 +1,13 @@
-L.Control.Spawn = L.Control.extend({
-    onAdd: function (map) {
-        var d = L.DomUtil.create('button');
-        L.DomEvent.on(d, 'click', () => map.setView([0, 0], 0));
-        d.textContent = 'Go to spawn';
-        return d;
-    },
-    onRemove: function (map) { }
-});
-
 L.control.spawn = function (opts) {
-    return new L.Control.Spawn(opts);
-}
+
+    let ctrl = L.Control.extend({
+        onAdd: map => {
+            var c = L.DomUtil.create('button');
+            L.DomEvent.on(c, 'click', () => map.setView([0, 0], 0));
+            c.textContent = 'Go to spawn';
+            return c;
+        }
+    });
+
+    return new ctrl(opts);
+};
