3b2079ff0630d0019521f8ac5e2a52939ca43d5f
Sean Buckley <sean.bck@gmail.com>
2022-01-15 23:45:18 -0500

    sort landmarks by Z coordinate, instead of time

--- control_beds.js
+++ control_beds.js
@@ -10,7 +10,7 @@ L.control.beds = function (opts) {
                 map.setView([-parseInt(coord[1]), parseInt(coord[0])]);
             });
 
-            for (let b of opts.beds.sort((a, b) => a.time - b.time)) {
+            for (let b of opts.beds.sort((a, b) => a.z - b.z)) {
                 let op = L.DomUtil.create('option');
                 op.value = b.x + ',' + b.z;
                 op.textContent =
--- control_signs.js
+++ control_signs.js
@@ -10,7 +10,7 @@ L.control.signs = function (opts) {
                 map.setView([-parseInt(coord[1]), parseInt(coord[0])]);
             });
 
-            for (let s of opts.signs.sort((a, b) => a.time - b.time)) {
+            for (let s of opts.signs.sort((a, b) => a.z - b.z)) {
                 let op = L.DomUtil.create('option');
                 op.value = s.x + ',' + s.z;
                 op.textContent =
